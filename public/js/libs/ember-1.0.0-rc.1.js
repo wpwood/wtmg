550b72729fc2a7cb0028962affd8df3e9a478f03
Bill Woodward <wpwood@gmail.com>
2013-02-19 23:09:18 -0500

    Fixed handlebars.js (previous version seems to have been incomplete)

--- public/js/libs/ember-1.0.0-rc.1.js
+++ public/js/libs/ember-1.0.0-rc.1.js
@@ -21418,7 +21418,7 @@ Ember.Handlebars.bootstrap = function(ctx) {
 
     var compile = (script.attr('type') === 'text/x-raw-handlebars') ?
                   Ember.$.proxy(Handlebars.compile, Handlebars) :
-                  Ember.$.proxy(Ember.Handlebars.precompile, Ember.Handlebars),
+                  Ember.$.proxy(Ember.Handlebars.compile, Ember.Handlebars),
       // Get the name of the script, used by Ember.View's templateName property.
       // First look for data-template-name attribute, then fall back to its
       // id if no name is found.
